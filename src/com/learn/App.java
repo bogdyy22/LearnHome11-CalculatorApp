b7d815e0e0609b3ec3de22ea37590bbbe02ce344
Bogdan Pacioianu <pacioianu4@gmail.com>
2021-11-25 21:01:30 +0200

    Initial commit

---
+++ src/com/learn/App.java
@@ -0,0 +1,54 @@
+package com.learn;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        double nr1, nr2;
+
+        try {
+            System.out.print("Insert number 1: ");
+            nr1 = sc.nextDouble();
+            sc.nextLine();
+
+            System.out.print("insert number 2: ");
+            nr2 = sc.nextDouble();
+            sc.nextLine();
+
+        } catch (InputMismatchException e) {
+            System.out.println("Couldn't format as a number");
+            return;
+        }
+
+        System.out.println("Insert the operator(+ - * / ): ");
+        var op = sc.nextLine();
+
+        double result;
+        switch (op) {
+            case "+":
+                result = nr1 + nr2;
+                break;
+            case "-":
+                result = nr1 - nr2;
+                break;
+            case "*":
+                result = nr1 * nr2;
+                break;
+            case "/":
+                result = nr1 / nr2;
+                break;
+            default:
+                System.out.println("Error chose operators valid :) ");
+                return;
+        }
+        System.out.println("The answer is " + result);
+
+
+    }
+
+
+}
